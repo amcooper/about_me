7637a8c895c7c9d623ac9a2ace51f0718b8dbe2b
Adam Cooper <amcooper@gmail.com>
2016-03-15 15:00:45 -0400

    Committing changes to scrolling redesign before starting shadowbox redesign.

--- js/index.js
+++ js/index.js
@@ -1,49 +1,51 @@
 $( document ).ready(function() {
 
-    // smoothDivScroll section
-    $("#ticker").smoothDivScroll({
-        mousewheelScrolling: "allDirections",
-        manualContinuousScrolling: true,
-        autoScrollingMode: "onStart"
-    });
-    
-    // These three animations scroll to the respective sections
-    $('#scroll_contact').on('click', function(event) {
-        event.preventDefault();
-        console.log($('#contact').offset().top);
-        $('html, body').animate({
-            scrollTop: $('#contact').offset().top - 50
-        }, 1000);
-    });    
+  // smoothDivScroll section
+  $("#ticker").smoothDivScroll({
+      mousewheelScrolling: "allDirections",
+      manualContinuousScrolling: true,
+      autoScrollingMode: "onStart"
+  });
+  
+  // Scrolling item event listeners
 
-    $('#scroll_projects').on('click', function(event) {
-        event.preventDefault();
-        console.log($('#projects').offset().top);
-        $('html, body').animate({
-            scrollTop: $('#projects').offset().top - 50
-        }, 1000);
-    });    
+  // // These three animations scroll to the respective sections
+  // $('#scroll_contact').on('click', function(event) {
+  //     event.preventDefault();
+  //     console.log($('#contact').offset().top);
+  //     $('html, body').animate({
+  //         scrollTop: $('#contact').offset().top - 50
+  //     }, 1000);
+  // });    
 
-    $('#scroll_about').on('click', function(event) {
-        event.preventDefault();
-        console.log($('#about').offset().top);
-        $('html, body').animate({
-            scrollTop: $('#about').offset().top - 50
-        }, 1000);
-    }); 
+  // $('#scroll_projects').on('click', function(event) {
+  //     event.preventDefault();
+  //     console.log($('#projects').offset().top);
+  //     $('html, body').animate({
+  //         scrollTop: $('#projects').offset().top - 50
+  //     }, 1000);
+  // });    
 
-    // Highlight clicked items in the navbar
-    $(".nav a").on("click", function(){
-        $(".nav").find(".active").removeClass("active");
-        $(this).parent().addClass("active");
-    });   
+  // $('#scroll_about').on('click', function(event) {
+  //     event.preventDefault();
+  //     console.log($('#about').offset().top);
+  //     $('html, body').animate({
+  //         scrollTop: $('#about').offset().top - 50
+  //     }, 1000);
+  // }); 
 
-    // Highlight project screenshots on mouseover
-    $(".screenshot").on("mouseenter", function(){
-        $( this ).css({ "border-color": "rgb(0,200,255)", "border-color": "rgba(0,200,255,0.5)" });
-    })
-    .on("mouseleave", function() {
-        $( this ).css({ "border-color": "transparent" });
-    });
+  // // Highlight clicked items in the navbar
+  // $(".nav a").on("click", function(){
+  //     $(".nav").find(".active").removeClass("active");
+  //     $(this).parent().addClass("active");
+  // });   
+
+  // // Highlight project screenshots on mouseover
+  // $(".screenshot").on("mouseenter", function(){
+  //     $( this ).css({ "border-color": "rgb(0,200,255)", "border-color": "rgba(0,200,255,0.5)" });
+  // })
+  // .on("mouseleave", function() {
+  //     $( this ).css({ "border-color": "transparent" });
+  // });
 
 });
