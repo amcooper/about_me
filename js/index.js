bbf9a98e98d795581599b7481d415cca6512111e
Adam Cooper <amcooper@gmail.com>
2016-03-27 18:24:22 -0400

    Hover working

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 $( document ).ready(function() {
 
+  $( '#aboutAdamLink' ).hover( function() {
+    $( '#aboutAdamHover' ).toggleClass( 'hidden' );
+  });
 
+  $( '#projectsLink' ).hover( function() {
+    $( '#projectsHover' ).toggleClass( 'hidden' );
+  });
 
 });
