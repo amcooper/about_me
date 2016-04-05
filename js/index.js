58c94d243aabd35179a8508ec318ea96701367c3
Adam Cooper <amcooper@gmail.com>
2016-04-05 13:11:42 -0400

    Navigation now works

--- js/index.js
+++ js/index.js
@@ -26,98 +26,60 @@ $( document ).ready(function() {
 
   parade();
 
-  // $( '#aboutAdamLink' ).hover( function() {
-  //   $( '#lowercase' ).toggleClass( 'ahidden' );
-  //   $( '#aboutAdamHover' ).toggleClass( 'ahidden' );
-  // });
-
-  // Consult this link:
-  // http://julian.com/research/velocity/#displayAndVisibility
-
-  $( '.hotAbout' ).hover( 
+  $( '#aboutAdamLink' ).hover( 
     function() {
-      console.log( 'about : hover on' );
-      $( '#lowercase' ).velocity( 'fadeOut' );
-      $( '#aboutAdamHover' ).velocity( 'fadeIn' );
-    }, function() {
-      console.log( 'about : hover off' );
-      $( '#lowercase' ).velocity( 'fadeIn' );
-      $( '#aboutAdamHover' ).velocity( 'fadeOut' );      
+      $( '#lineTwo' ).css( 'opacity', '0.0' );
+      $( '.willHide' ).css( 'opacity', '0.0' );
+      $(this).html( "about_adam" ); 
+      $(this).addClass("aboutAdamHover"); 
+    },
+    function() {
+      $(this).removeClass("aboutAdamHover");
+      $(this).html( "a" );
+      $( '#lineTwo' ).css( 'opacity', '1.0' );
+      $( '.willHide' ).css( 'opacity', '1.0' );
     }
   );
 
+  $( '#projectsLink' ).hover(
+    function() {
+      $( '#lineOne' ).css( 'opacity', '0.0' );
+      $( '.willHide' ).css( 'opacity', '0.0' );
+      $(this).html( "projects" ); 
+      $(this).addClass("projectsHover"); 
+    },
+    function() {
+      $(this).removeClass("projectsHover");
+      $(this).html( "p" );
+      $( '#lineOne' ).css( 'opacity', '1.0' );
+      $( '.willHide' ).css( 'opacity', '1.0' );
+    }
+  );
 
-// // // This seems to work (on Firefox) but is slow:
-  
-  // $( '#aboutAdamLink' ).mouseover( function() {
-  //     console.log( 'aboutAdamLink : mouseover' );
-  //     $( '#lowercase' ).velocity( 'fadeOut' );
-  //     $( '#aboutAdamHover' ).velocity( 'fadeIn' );
-  // });
-
-  // $( '#aboutAdamHover' ).mouseout( function() {
-  //     console.log( 'aboutAdamHover : mouseout' );
-  //     $( '#lowercase' ).velocity( 'fadeIn' );
-  //     $( '#aboutAdamHover' ).velocity( 'fadeOut' );
-  // });  
-
-
-// // // This doesn't work on either browser.
-
-  // $( '#aboutAdamLink' ).mouseenter( function() {
-  //     console.log( 'aboutAdamLink : mouseenter' );
-  //     $( '#lowercase' ).velocity( 'fadeOut' );
-  //     $( '#aboutAdamHover' ).velocity( 'fadeIn' );
-  // });
-
-  // $( '#aboutAdamHover' ).mouseleave( function() {
-  //     console.log( 'aboutAdamHover : mouseleave' );
-  //     $( '#lowercase' ).velocity( 'fadeIn' );
-  //     $( '#aboutAdamHover' ).velocity( 'fadeOut' );
-  // });  
-
-  // $( '#projectsLink' ).hover( function() {
-  //   console.log( 'projectsLink : hover' );
-  //   $( '#lowercase' ).velocity( 'fadeOut' );
-  //   $( '#projectsHover' ).velocity( 'fadeIn' );
-  // });
-
-  // $( '#projectsHover' ).hover( function() {
-  //   console.log( 'projectsHover : hover' );
-  //   $( '#lowercase' ).velocity( 'fadeOut' );
-  //   $( '#projectsHover' ).velocity( 'fadeIn' );
-  // });
-
-  $( '#aboutAdamHover' ).click( function() {
-    console.log( 'aboutAdamHover : click' );
-    $( '#lowercase' ).velocity( 'fadeOut', { duration: 1000 });
-    $( '#aboutAdamHover' ).velocity( 'fadeOut', { duration: 1000 });
-    $( '#aboutAdamPanel' ).velocity( 'fadeIn', { duration: 1000 });
+  $( '#aboutAdamLink' ).click( function() {
+    $( '#lowercase' ).css('z-index', '1').velocity('fadeOut', { duration: 1000 });
+    $( '#aboutAdamPanel' ).css('z-index', '3').velocity('fadeIn', { duration: 1000 });
   });
 
-  $( '#projectsHover' ).click( function() {
-    console.log( 'projectsHover : click' );
-    $( '#lowercase' ).velocity( 'fadeOut', { duration: 1000 });
-    $( '#projectsHover' ).velocity( 'fadeOut', { duration: 1000 });
-    $( '#projectsPanel' ).velocity( 'fadeIn', { duration: 1000 });
+  $( '#projectsLink' ).click( function() {
+    $( '#lowercase' ).css('z-index', '1').velocity('fadeOut', { duration: 1000 });
+    $( '#projectsPanel' ).css('z-index', '3').velocity('fadeIn', { duration: 1000 });
   });
 
   $( '.navHome' ).click( function() {
-    console.log( 'navHome : click' );
-    $( '.panel' ).velocity( 'fadeOut', { duration: 1000 });
-    $( '#lowercase' ).velocity( 'fadeIn', { duration: 1000 });
+    $( '#aboutAdamPanel' ).css('z-index', '1').velocity( 'fadeOut', { duration: 1000 });
+    $( '#projectsPanel' ).css('z-index', '1').velocity( 'fadeOut', { duration: 1000 });
+    $( '#lowercase' ).css('z-index', '3').velocity( 'fadeIn', { duration: 1000 });
   });
 
   $( '#navProjects' ).click( function() {
-    console.log( 'navProjects : click' );
-    $( '#aboutAdamPanel' ).velocity( 'fadeOut', { duration: 1000 });
-    $( '#projectsPanel' ).velocity( 'fadeIn', { duration: 1000 });
+    $( '#aboutAdamPanel' ).css('z-index', '1').velocity( 'fadeOut', { duration: 1000 });
+    $( '#projectsPanel' ).css('z-index', '3').velocity( 'fadeIn', { duration: 1000 });
   });
 
   $( '#navAbout' ).click( function() {
-    console.log( 'navAbout : click' );
-    $( '.panel' ).velocity( 'fadeOut', { duration: 1000 });
-    $( '#aboutAdamPanel' ).velocity( 'fadeIn', { duration: 1000 });
+    $( '#projectsPanel' ).css('z-index', '1').velocity( 'fadeOut', { duration: 1000 });
+    $( '#aboutAdamPanel' ).css('z-index', '3').velocity( 'fadeIn', { duration: 1000 });
   });
 
 });
