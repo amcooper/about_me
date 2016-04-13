496dc1c3c4dabc192725154ba4e353549af7eda5
Adam Cooper <amcooper@gmail.com>
2016-04-13 13:22:01 -0400

    Colored contact icons. Attempt to repair click events. Prob other stuff.

--- js/index.js
+++ js/index.js
@@ -56,22 +56,13 @@ $( document ).ready(function() {
     }
   );
 
-  // $( '#aboutLink' ).click( function() {
-  //   $( '#lowercase' ).css('z-index', '1').velocity('fadeOut', { duration: 1000 });
-  //   $( 'video' ).css( 'opacity', '0.0' );
-  //   $( '.container-fluid' ).css({ 'width':'3840px', 'left':'-1280px' });
-  //   $( '.triptych' ).css('z-index', '3');
-  //   $( '#middlePanel' ).velocity('fadeIn', { duration: 1000 });
-  //   $( '#floatingNav' )
-  //     .css('z-index', '5')
-  //     .velocity({right: 'auto', left: '500px'})
-  //     .velocity('fadeIn', { duration: 1000 });
-  // });
 
   $( '#aboutLink' ).click( function() {
     $.Velocity.RunSequence([
       { e: $( '#lowercase' ), p: { zIndex: 1 }, o: { duration: 1 } },
       { e: $( '#lowercase' ), p: 'fadeOut', o: { duration: 400, sequenceQueue: false } },
+      { e: $( '#projectsPanel' ), p: { zIndex: 1 }, o: { duration: 1, sequenceQueue: false } },
+      { e: $( '#projectsPanel' ), p: 'fadeOut', o: { duration: 1000, sequenceQueue: false } },
       { e: $( 'video' ), p: { opacity: 0.0 }, o: { duration: 400, sequenceQueue: false } },
       { e: $( '.container-fluid' ), p: { width: '3840px', left: '-1280px' }, o: { duration: 1 } },
       { e: $( '.triptych' ), p: { zIndex: 3 }, o: { duration: 1, sequenceQueue: false } },
@@ -84,51 +75,75 @@ $( document ).ready(function() {
 
   $( '#fHomeLink' ).hover(
     function() {
-      $( this ).html('home');
+      $( this ).html('<a href="#">home</a>');
     },
     function() {
-      $( this ).html('&bull;');
+      $( this ).html('<a href="#">&bull;</a>');
     }
   );
 
-  $( '#fHomeLink' ).click( function() {});
+  $( '#fHomeLink' ).click( function() {
+    $( this ).html('<a href="#">&bull;</a>');
+    console.log('fAboutLink : clicked.'); // debug
+    $( '#floatingNav' ).css('z-index', '1').velocity('fadeOut', { duration: 1000 });
+    $( '.triptych' ).css('z-index', '1').velocity('fadeOut', { duration: 1000 });
+    $( '.container-fluid' ).css({ 'width':'100%', 'left': '0' });
+    $( 'video' ).css( 'opacity', '1.0' );
+    $( '#projectsPanel' ).css( 'z-index', '1').velocity('fadeOut', { duration: 1000 });
+    $( '#lowercase' ).css( 'z-index', '3' ).velocity('fadeIn', { duration: 1000 });
+  });
 
   $( '#fAboutLink' ).hover(
     function() {
-      $( this ).html('about');
+      $( this ).html('<a href="#">about</a>');
     },
     function() {
-      $( this ).html('&bull;');
+      $( this ).html('<a href="#">&bull;</a>');
     }
   );
 
-  $( '#fAboutLink' ).click( function() {});
+  $( '#fAboutLink' ).click( function() {
+    $( this ).html('<a href="#">&bull;</a>');
+    console.log('fAboutLink : clicked.'); // debug
+    $( '#middlePanel' ).velocity('fadeIn', { duration: 1000 });
+    $( '.container-fluid' ).velocity({left: '-1280px'}, { duration: 1000, easing: [250,50] });
+    $( '#photoPanel,#bioPanel' ).velocity('fadeOut', { duration: 1000 });
+    $( '#projectsPanel' ).css( 'z-index', '1').velocity('fadeOut', { duration: 1000 });
+    $( '#floatingNav' ).velocity({left: '500px'});
+  });
 
   $( '#fProjectsLink' ).hover(
     function() {
-      $( this ).html('projects');
+      $( this ).html('<a href="#">projects</a>');
     },
     function() {
-      $( this ).html('&bull;');
+      $( this ).html('<a href="#">&bull;</a>');
     }
   );
 
-  $( '#fProjectsLink' ).click( function() {});
+  $( '#fProjectsLink' ).click( function() {
+    $( this ).html('<a href="#">&bull;</a>');
+    console.log( 'projects -> •');
+    $( '#floatingNav' ).velocity({ left: '1%' });
+    $( '.triptych' ).css('z-index', '1').velocity('fadeOut', { duration: 1000 });
+    $( '.container-fluid' ).css({ 'width':'100%', 'left': '0' });
+    $( '#projectsPanel' ).css('z-index', '3').velocity('fadeIn', { duration: 1000 });
+  });
 
   $( '#fLateralLink' ).hover(
     function() {
-      $( '#fPhotoLink' ).html('photo&nbsp;&lt;');
-      $( '#fBioLink' ).html('&gt;&nbsp;bio');
+      $( '#fPhotoLink' ).html('<a href="#">photo&nbsp;&lt;</a>');
+      $( '#fBioLink' ).html('<a href="#">&gt;&nbsp;bio</a>');
     },
     function() {
-      $( '#fPhotoLink' ).html('&lt;');
-      $( '#fBioLink' ).html('&gt;');
+      $( '#fPhotoLink' ).html('<a href="#">&lt;</a>');
+      $( '#fBioLink' ).html('<a href="#">&gt;</a>');
     }
   );
 
   $( '#fGmailShow' ).hover(
     function() {
-      $( this ).html('<img src="images/gmail-64-black.png" target="_blank">');
+      $( this ).html('<img src="images/gmail-64-color.png" target="_blank">');
     },
     function() {
       $( this ).html('&bull;');
@@ -137,7 +152,7 @@ $( document ).ready(function() {
 
   $( '#fGitHubShow' ).hover(
     function() {
-      $( this ).html('<img src="images/github-64-black.png" target="_blank">');
+      $( this ).html('<img src="images/github-64-color.png" target="_blank">');
     },
     function() {
       $( this ).html('&bull;');
@@ -146,7 +161,7 @@ $( document ).ready(function() {
 
   $( '#fLinkedInShow' ).hover(
     function() {
-      $( this ).html('<img src="images/linkedin-64-black.png" target="_blank">');
+      $( this ).html('<img src="images/linkedin-64-color.png" target="_blank">');
     },
     function() {
       $( this ).html('&bull;');
@@ -154,6 +169,10 @@ $( document ).ready(function() {
   );
 
   $( '#fPhotoLink' ).click( function() {
+    $( '#fPhotoLink' ).html('<a href="#">&lt;</a>');
+    $( '#fBioLink' ).html('<a href="#">&gt;</a>');
+    console.log('fPhotoLink : clicked.'); // debug
+    $( '#projectsPanel' ).css( 'z-index', '1').velocity('fadeOut', { duration: 1000 });
     $( '#photoPanel' ).velocity('fadeIn', { duration: 1000 });
     $( '.container-fluid' ).velocity({left: '0'}, { duration: 1000, easing: [250,50] });
     $( '#middlePanel,#bioPanel' ).velocity('fadeOut', { duration: 1000 });
@@ -162,6 +181,10 @@ $( document ).ready(function() {
   });
 
   $( '#fBioLink' ).click( function() {
+    $( '#fPhotoLink' ).html('<a href="#">&lt;</a>');
+    $( '#fBioLink' ).html('<a href="#">&gt;</a>');
+    console.log('fBioLink : clicked.'); // debug
+    $( '#projectsPanel' ).css( 'z-index', '1').velocity('fadeOut', { duration: 1000 });
     $( '#bioPanel' ).velocity('fadeIn', { duration: 1000 });
     $( '.container-fluid' ).velocity({left: '-2560px'}, { duration: 1000, easing: [250,50] });
     $( '#middlePanel,#photoPanel' ).velocity('fadeOut', { duration: 1000 });
@@ -170,6 +193,10 @@ $( document ).ready(function() {
   });
 
   $( '#fAboutSlash' ).click( function() {
+    $( '#fPhotoLink' ).html('<a href="#">&lt;</a>');
+    $( '#fBioLink' ).html('<a href="#">&gt;</a>');
+    console.log('fAboutSlash : clicked.'); // debug
+    $( '#projectsPanel' ).css( 'z-index', '1').velocity('fadeOut', { duration: 1000 });
     $( '#middlePanel' ).velocity('fadeIn', { duration: 1000 });
     $( '.container-fluid' ).velocity({left: '-1280px'}, { duration: 1000, easing: [250,50] });
     $( '#photoPanel,#bioPanel' ).velocity('fadeOut', { duration: 1000 });
@@ -180,6 +207,9 @@ $( document ).ready(function() {
     $( '#lowercase' ).css('z-index', '1').velocity('fadeOut', { duration: 1000 });
     $( 'video' ).css( 'opacity', '0.0' );
     $( '#projectsPanel' ).css('z-index', '3').velocity('fadeIn', { duration: 1000 });
+    $( '#floatingNav' )
+      .css({'z-index': '5', right: 'auto', left: '1%' })
+      .velocity('fadeIn', { duration: 1000 });
   });
 
 });
