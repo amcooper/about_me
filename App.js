307ae916f15abd688adb796ab64a2f512cc80130
Adam Cooper <amcooper@gmail.com>
2017-08-04 03:14:20 -0400

    Committing before branch operations

--- App.js
+++ App.js
@@ -13,14 +13,62 @@ function Backdrop(props) {
 }
 
 function Navbar(props) {
-  
-}
+  return (
+    <nav id="floatingNav" className="nn">
+      <div id="fNavTop">
+        <div id="fHomeLink" className="navLink">
+          <a href="#" className="navBullet" id="homeHoverHide">&bull;</a>
+          <a href="#" className="navText" id="homeHoverShow">home</a>
+        </div>
+        <div id="fProjectsLink" className="navLink">
+          <a href="#" className="navBullet" id="projectsHoverHide">&bull;</a>
+          <a href="#" className="navText" id="projectsHoverShow">projects</a>
+        </div>
+      </div>
+
+      <div id="fLateralLink" className="navLink">
+        <div id="fPhotoLink">
+          <a href="#" className="navBullet lateralHoverHide">&lt;</a>
+          <a href="#" className="lateralHoverShow"><span className="navText">photo&nbsp;</span><span className="navBullet">&lt;</span></a>
+        </div>
+        <div id="fAboutSlash">
+          <a href="#" className="navBullet">/</a>
+        </div>
+        <div id="fBioLink">
+          <a href="#" className="navBullet lateralHoverHide">&gt;</a>
+          <a href="#" className="lateralHoverShow"><span className="navBullet">&gt;</span><span className="navText">&nbsp;bio</span></a>
+        </div>
+      </div>
+
+      <div id="fContactLinks" className="navLink">
+        <ul>
+          <li id="fGmailLink">
+            <div id="gmailHoverHide" className="navBullet">&bull;</div>
+            <a href="mailto:amcooper+website@gmail.com" title="Gmail" target="_blank"><img src="images/gmail-64-color.png" id="gmailHoverShow" /></a>
+          </li>
+          <li id="fGithubLink">
+            <div id="githubHoverHide" className="navBullet">&bull;</div>
+            <a href="https://github.com/amcooper" title="GitHub" target="_blank"><img src="images/github-64-color.png" id="githubHoverShow" /></a>
+          </li>
+          <li id="fLinkedinLink">
+            <div id="linkedinHoverHide" className="navBullet">&bull;</div>
+            <a href="https://linkedin.com/in/adamcooperdev" title="LinkedIn" target="_blank"><img src="images/linkedin-64-color.png" id="linkedinHoverShow" /></a>
+          </li>
+          <li id="fWordpressLink">
+            <div id="wordpressHoverHide" className="navBullet">&bull;</div>
+            <a href="https://serverdotjive.wordpress.com" title="Wordpress" target="_blank"><img src="images/wordpress-64-color.png" id="wordpressHoverShow" alt="server-dot-jive" /></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    ); 
+};
 
 function Home(props) {
 	return (
     <div>
   		<div id="landing">
-  	    <p className="nt" id="lineOne"><span className="hot hotAbout" id="aboutLink"><a href="#/about">a</a></span><span className="willHide">dam_</span></p>
+  	    <p className="nt" id="lineOne"><span className="hot hotAbout" id="aboutLink" onClick={() => props.onClick("about")}>a</span><span className="willHide">dam_</span></p>
   	    <p className="nt" id="lineTwo"><span className="willHide">coo</span><span className="hot hotProjects" id="projectsLink" onClick={() => props.onClick("projects")}>p</span><span className="willHide">er</span></p>
   	    <p className="nm willHide"><span className="underline ahidden">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span className="ticker">.js</span><span className="ticker">.html</span><span className="ticker">.css</span><span className="ticker">.rb</span><span className="ticker jive"><a href="jive.html" target="_blank">.jive</a></span></p>	
       </div>
@@ -29,12 +77,25 @@ function Home(props) {
 	);
 };
 
-function About(props) {
-  return (
-    <div id="bioText">
-      <p className="longText">A map is not just a map. It’s more of a conversation between the mapmaker and the map reader—and the landscape. As a student in Computer Sciences and Cartography at the University of Wisconsin, I started out exploring the intersection between maps and code. But after making maps in Scotland, writing a book, managing tech teams at companies with vastly different missions and vibes, editing a database of secret (but not entirely top secret) cutting edge drug trials, and nursing a linguistics habit on the side, I am now crafting maps out of code. I’m the developer. My users and students are the readers. The landscape is the web. Let’s <span id="talk">talk</span>.</p>
-    </div>
-  )
+class About extends Component {
+  componentWillMount( callback ) {
+    alert('Enter: about');
+  }
+
+  componentWillUnmount( callback ) {
+    alert('Leave: about');
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="bioText">
+          <p className="longText">A map is not just a map. It’s more of a conversation between the mapmaker and the map reader—and the landscape. As a student in Computer Sciences and Cartography at the University of Wisconsin, I started out exploring the intersection between maps and code. But after making maps in Scotland, writing a book, managing tech teams at companies with vastly different missions and vibes, editing a database of secret (but not entirely top secret) cutting edge drug trials, and nursing a linguistics habit on the side, I am now crafting maps out of code. I’m the developer. My users and students are the readers. The landscape is the web. Let’s <span id="talk">talk</span>.</p>
+        </div>
+        <Navbar />
+      </div>
+    );
+  }
 };
 
 function Projects(props) {
@@ -59,6 +120,7 @@ function Projects(props) {
           <p className="longText">A library and e-reader. Built with JavaScript and Node.js.</p>
         </div>
       </div>
+      <Navbar />
     </div>
   );
 };
@@ -75,7 +137,7 @@ class App extends Component {
 
   handleClick( panel ) {
     let newState = {};
-    alert(`Panel: ${panel}`);
+    // alert(`Panel: ${panel}`); // debug
     if ( panel === "projects") { 
       newState = { home: false, about: false, projects: true };
     } else if ( panel === "about" ) {
